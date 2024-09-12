1e8b683b7b404445f1dc9285108cac1df8c486ae
LeeCoder-beginner <76464923+LeeCoder-beginner@users.noreply.github.com>
2024-09-12 16:09:35 +0900

    Title: 다양하게 출력, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240912/다양하게 출력/print-in-variety.py
@@ -0,0 +1,9 @@
+print("Total days in Year\n", 365, sep="")
+print("Circumference rate\n", 3.1415926535, sep="")
+
+### print("Total days in Year")
+### print(365)
+### print(Circumference rate)
+### print(3.1415926535)
+
+### print("Total days in Year\n", 365, "Circumference rate\n", 3.1415926535, sep="")
