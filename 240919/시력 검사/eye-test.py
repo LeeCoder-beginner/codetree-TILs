84ec7fe0a92171a615883d51ceafa3ee10399d07
LeeCoder-beginner <76464923+LeeCoder-beginner@users.noreply.github.com>
2024-09-19 20:05:31 +0900

    Title: 시력 검사, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240919/시력 검사/eye-test.py
+++ 240919/시력 검사/eye-test.py
@@ -3,7 +3,7 @@ right = float(input())
 
 if left >= 1,0 and right >= 1.0:
     print("High")
-elif lef >= 0.5 and right >= 0.5:
+elif left >= 0.5 and right >= 0.5:
     print("Middle")
 else:
     print("Low")
