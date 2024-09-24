cb0c5d19e665d7ec7a9c0b50d0571d048f6e990d
LeeCoder-beginner <76464923+LeeCoder-beginner@users.noreply.github.com>
2024-09-24 16:46:23 +0900

    Title: 1부터의 합, Time: 148ms, Memory: 24MB, Status: Passed - CodeTree

--- 240924/1부터의 합/sum-from-the-one.py
+++ 240924/1부터의 합/sum-from-the-one.py
@@ -1,9 +1,10 @@
 n = int(input())
 sum_val = 0
+cnt = 0
 for i in range(1, 101):
-    sum_val += i
     if sum_val >= n:
-        sum_val -= i
         break
+    sum_val += i
+    cnt += 1
 
-print(sum_val)
+print(cnt)
