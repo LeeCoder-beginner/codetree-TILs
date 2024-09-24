d0e2daf4e6cc30da728bdda4e63b620c6a03b3ed
LeeCoder-beginner <76464923+LeeCoder-beginner@users.noreply.github.com>
2024-09-24 16:57:45 +0900

    Title: 1까지 나누기, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240924/1까지 나누기/divide-by-1.py
+++ 240924/1까지 나누기/divide-by-1.py
@@ -1,7 +1,18 @@
 n = int(input())
 
-for i in range(1, n+1):
+# for i in range(1, n+1):
+#     n //= i
+#     if n <= 1:
+#         print(i)
+#         break
+
+## 2 while 문
+cnt = 0
+i = 1
+
+while n > 1:
     n //= i
-    if n <= 1:
-        print(i)
-        break
+    i += 1
+    cnt += 1
+    
+print(i-1)
