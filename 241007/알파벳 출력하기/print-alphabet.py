7b0d13444bf95c7de7b9e44902fb9bb0a1b5670a
LeeCoder-beginner <76464923+LeeCoder-beginner@users.noreply.github.com>
2024-10-07 17:25:21 +0900

    Title: 알파벳 출력하기, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 241007/알파벳 출력하기/print-alphabet.py
+++ 241007/알파벳 출력하기/print-alphabet.py
@@ -1,12 +1,20 @@
 n = int(input())
 cnt = 'A'
 
+# for i in range(n):
+#     for j in range(i+1):
+#         if cnt == 'Z': # 알파벳을 출력하는 경우이기 때문에 Z가 나오면 출력하고 cnt를 A로 바꾸어주는 과정이 필요
+#             print(cnt, end="") 
+#             cnt = 'A'
+#         else:
+#             print(cnt, end="")
+#             cnt = chr(ord(cnt) + 1)
+#     print()
+
 for i in range(n):
     for j in range(i+1):
-        if cnt == 'Z': # 알파벳을 출력하는 경우이기 때문에 Z가 나오면 출력하고 cnt를 A로 바꾸어주는 과정이 필요
-            print(cnt, end="") 
+        print(cnt, end="")
+        cnt = chr(ord(cnt) + 1)
+        if ord(cnt) > ord('Z'): # 알파벳을 출력하는 경우이기 때문에 Z가 나오면 출력하고 cnt를 A로 바꾸어주는 과정이 필요
             cnt = 'A'
-        else:
-            print(cnt, end="")
-            cnt = chr(ord(cnt) + 1)
     print()
