43ddc403124c3d29d660ca22da4f4175c94f9cb3
LeeCoder-beginner <76464923+LeeCoder-beginner@users.noreply.github.com>
2024-10-07 17:33:05 +0900

    Title: ab곱 n번 구하기, Time: 128ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 241007/ab곱 n번 구하기/a-multiple-b-n-times.py
@@ -0,0 +1,10 @@
+n = int(input())
+mul_val = 1
+
+for i in range(n):
+    a, b = map(int, input().split())
+    
+    mul_val = 1
+    for j in range(a, b+1):
+        mul_val *= j
+    print(mul_val)
