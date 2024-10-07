ad7a124be8e04773ea6449715ad565fc78d8d522
LeeCoder-beginner <76464923+LeeCoder-beginner@users.noreply.github.com>
2024-10-07 17:56:58 +0900

    Title: 약수가 세 개인 수, Time: 126ms, Memory: 25MB, Status: Passed - CodeTree

--- 241007/약수가 세 개인 수/numbers-with-three-divisors.py
+++ 241007/약수가 세 개인 수/numbers-with-three-divisors.py
@@ -2,7 +2,7 @@ start, end = map(int, input().split())
 meo = 0
 for i in range(start, end+1):
     cnt = 0
-    for j in range(1, i):
+    for j in range(1, i+1):
         if i % j == 0:
             cnt += 1
     if cnt == 3:
