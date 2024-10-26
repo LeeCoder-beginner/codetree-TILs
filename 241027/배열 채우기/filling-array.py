206bf437fc7784dcb3a118457b94c838202df6bb
LeeCoder-beginner <76464923+LeeCoder-beginner@users.noreply.github.com>
2024-10-27 00:04:01 +0900

    Title: 배열 채우기, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 241027/배열 채우기/filling-array.py
+++ 241027/배열 채우기/filling-array.py
@@ -1,7 +1,14 @@
 list1 = list(map(int, input().split()))
 
-for i in range(len(list1)-1, -1, -1):
+# 0이 나오기 전까지 저장해주는 list 만들기
+list2 = []
+
+for i in range(len(list1)):
     if list1[i] == 0:
-        continue
+        break
     else:
-        print(list1[i], end=" ")
+        list2.append(list1[i])
+
+# 0dl 나오기 전까지 저장되어 있는 list를 역순으로 출력하기
+for i in range(len(list2)-1, -1, -1):
+    print(list1[i], end=" ")
